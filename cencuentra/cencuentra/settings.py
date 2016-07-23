cc3e8e9da1084a8065d3a806dae63fb3c1c821c1
David Moranchel <david.arcshtag@gmail.com>
2016-07-23 16:49:11 -0500

    create app negocios an add in settings

--- cencuentra/cencuentra/settings.py
+++ cencuentra/cencuentra/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    #apps
+    'negocios',
     #api
     'api',
     #Rest Framework
