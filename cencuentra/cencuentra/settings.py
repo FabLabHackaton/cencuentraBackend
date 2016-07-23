b958b3ea7122a641bf7e62f71407cb6087f32063
David Moranchel <david.arcshtag@gmail.com>
2016-07-23 16:21:40 -0500

    create app api and api/urls.py, add in settings api and restframework

--- cencuentra/cencuentra/settings.py
+++ cencuentra/cencuentra/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    #api
+    'api',
+    #Rest Framework
+    'rest_framework',
 )
 
 MIDDLEWARE_CLASSES = (
